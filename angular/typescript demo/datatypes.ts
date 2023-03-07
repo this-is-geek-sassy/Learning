af85b94c347f22079ffc5c98fd1b82a2cd03b4ac
saswata-mishra_psl <saswata.mishra.cse21@heritageit.edu.in>
2023-03-07 18:17:57 +0530

    number type

--- angular/typescript demo/datatypes.ts
+++ angular/typescript demo/datatypes.ts
@@ -5,4 +5,19 @@ let newName = lname.toUpperCase();
 
 console.log(newName);
 
+// numbers
+let age: number;
+age = 23;
+// age = "23";  // gives error
+age = 23.5;
+
+let result = age.toString();
+let result2 = parseFloat("34.6");
+console.log(result, result2);
+console.log(result2);
+
+let dob = "25";
+let result3 = parseInt(dob);
+console.log(result3);
+
 // lname = 76;
