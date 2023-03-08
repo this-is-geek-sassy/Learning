09c8af8198a4b5b29802a1e9415ea0917f58f920
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2023-03-08 20:21:30 +0530

    created alt login component and deletd prev one

--- angular_csbs/blog/src/app/app.module.ts
+++ angular_csbs/blog/src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+// import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserPrefComponent } from './user-pref/user-pref.component';
 import { StudentPrefComponent } from './student-pref/student-pref.component';
 import { CountryListComponent } from './country-list/country-list.component';
 
+import { UserAuth2Module } from "./user-auth2/user-auth2.module";
+
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+    // LoginComponent,
     RegisterComponent,
     UserListComponent,
     UserPrefComponent,
@@ -20,7 +22,8 @@ import { CountryListComponent } from './country-list/country-list.component';
     CountryListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserAuth2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
