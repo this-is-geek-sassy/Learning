4a3bd9db2561f53cfa234808101b47941a4f208e
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2023-03-08 19:21:49 +0530

    angular component

--- angular_csbs/blog/src/app/app.module.ts
+++ angular_csbs/blog/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
