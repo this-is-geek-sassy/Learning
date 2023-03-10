6716639a4ac4caeec5d4128001d63c4f3ef366cd
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2023-03-10 17:21:44 +0530

    ngfor

--- angular_csbs/blog/src/app/user-auth2/login/login.component.ts
+++ angular_csbs/blog/src/app/user-auth2/login/login.component.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-  name: string = 'peter';
-  disablePasswd: boolean = false;
+  // name: string = 'peter';
+  // disablePasswd: boolean = false;
+
+  users: Array<string> = ['anil', 'bhaskar', 'sam', 'peter', 'bruce'];
+
+  userDetails: Array<object> = [
+    {
+      name: this.users[0],
+      email: this.users[0] + "@gmail.com",
+      phoneNumber: "6454"
+    },
+    {
+      name: this.users[1],
+      email: this.users[1] + "@gmail.com",
+      phoneNumber: "6453"
+    },
+    {
+      name: this.users[2],
+      email: this.users[2] + "@gmail.com",
+      phoneNumber: "6452"
+    },
+    {
+      name: this.users[3],
+      email: this.users[3] + "@gmail.com",
+      phoneNumber: "6450"
+    },
+    {
+      name: this.users[4],
+      email: this.users[4] + "@gmail.com",
+      phoneNumber: "6459"
+    }
+  ];
 }
