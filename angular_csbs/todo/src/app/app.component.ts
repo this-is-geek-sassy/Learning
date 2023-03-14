ff3cd37cffb17b6fd46ff87981a4339b19507181
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2023-03-14 12:26:40 +0530

    made todo list using bootstrap

--- angular_csbs/todo/src/app/app.component.ts
+++ angular_csbs/todo/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'Todo list on Anguar';
+
+  taskList: any[] = [];
+
+  addTask(item: string) {
+    // console.log(item);
+    this.taskList.push({id: this.taskList.length, name: item});
+    console.log(this.taskList);
+  }
+
+  removeTask(id: number) {
+    this.taskList = this.taskList.filter(item => item.id !== id);
+  }
 }
