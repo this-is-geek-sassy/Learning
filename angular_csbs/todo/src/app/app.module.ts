8f962bf9d07ad7ca5e00c47da7c6748c65ed561d
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2023-03-20 19:14:29 +0530

    css learning (20.3.23)

--- angular_csbs/todo/src/app/app.module.ts
+++ angular_csbs/todo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ChildComponent } from './child/child.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
