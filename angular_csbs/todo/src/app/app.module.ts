2f5bd0c3a80b941093578e37901ae04aba09393c
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2023-04-05 16:27:47 +0530

    custom pipes

--- angular_csbs/todo/src/app/app.module.ts
+++ angular_csbs/todo/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
 import { Child2Component } from './child2/child2.component';
 
 import { FormsModule } from "@angular/forms";
+import { UsdToInrPipe } from './custom-pipes/usd-to-inr.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChildComponent,
     UserDetailComponent,
-    Child2Component
+    Child2Component,
+    UsdToInrPipe
   ],
   imports: [
     BrowserModule,
