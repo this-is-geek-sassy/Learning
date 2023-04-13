24b564eb22a86b8eb05cc5c970d293c0d53ba570
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2023-04-13 19:29:42 +0530

    recative forms intro

--- angular_csbs/blog-2/src/app/app.component.ts
+++ angular_csbs/blog-2/src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 
+import { FormControl, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog-2';
+  title = 'template driven form: ';
+  title2 = 'reactive form: ';
+
+  loginForm = new FormGroup({
+    user: new FormControl(this.title2),   // default values set
+    password: new FormControl('saswata')
+  });
 
   userLogin(item:any): void {
     console.log(item);
   }
+
+  login(): void {
+    console.log(this.loginForm.value);
+  }
+
+  // in the html file it is not recomended to mix template driven form and reactive forms: https://stackoverflow.com/questions/55739509/mixing-reactive-form-with-template-form 
 }
