678a1b300aa2449f5da68acb4d1e4a6eec884b61
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2023-10-04 18:19:00 +0530

    gen ai python

---
+++ Python/hello.py
@@ -0,0 +1,46 @@
+import math as m
+
+print ("Hello world")
+
+print (2+2)
+
+print (2**2)
+
+print (2**3)
+
+print (5/2)
+
+print (m.floor(5/2));  print(5//2)
+
+# print(1.000.000.000)
+print(1,000,000);  print(10000000)
+
+print ("what's up")
+print ('what\'s up')
+
+# comment (one line)
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+# ----------------------
+# variable
+red_bucket = 'kevin';  # string type
+red_bucket = 10;  # integer type
+print(red_bucket)
+print(type(red_bucket))
+# del red_bucket   # this is how you delete a variable
+# print(red_bucket)
+# print(type(red_bucket))
+
+# how to take input
+red_bucket = input("What do u wanna put in the bucket: ")
+print(red_bucket)
+
+
+# conditional logic
+# operator:
+print("Is 5=4 : ", 5==4)
+print("Is 5!=4 : ", 5!=4)
---
+++ Python/hello2.py
@@ -0,0 +1,6 @@
+thomas_age = 3
+age_at_kindergarden = 5
+
+print("thomas age ?= age at kindergarden : ", thomas_age==age_at_kindergarden)
+
+# source: https://youtu.be/b093aqAZiPU?si=VQwj0sa9UMROZ7hx&t=2104
