63e04fe768041a03586fcfe6a665c804b5f55052
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2023-10-25 19:01:50 +0530

    more kwargs

--- Python/kwargs.py
+++ Python/kwargs.py
@@ -1,4 +1,4 @@
-def parrot(voltage:int, state='a stiff', action='voom', type='Norwegian Blue'):
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
     print("-- This parrot wouldn't", action, end=' ')
     print("if you put", voltage, "volts through it.")
     print("-- Lovely plumage, the", type)
@@ -9,3 +9,12 @@ print("------")
 parrot(voltage=1000)
 print("----------")
 parrot(voltage=1000, action="Voooom")
+print("========")
+parrot(action='VOOOOOM', voltage=1000000)
+print("$$$$$$")
+parrot('a million', 'bereft of life', 'jump')
+print("#########")
+parrot('a thousand', state='pushing up the daisies')
+
+# invalid calls
+parrot(voltage=5.0, 'dead')
