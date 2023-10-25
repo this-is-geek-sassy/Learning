ec42191238cdbd12f68062dc4fefe97f7992fd45
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2023-10-25 18:03:00 +0530

    kwargs

---
+++ Python/kwargs.py
@@ -0,0 +1,11 @@
+def parrot(voltage:int, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+
+parrot(1000)
+print("------")
+parrot(voltage=1000)
+print("----------")
+parrot(voltage=1000, action="Voooom")
