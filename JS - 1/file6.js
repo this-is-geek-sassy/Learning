4884ed0f810575749c35643d6585ee9c104d5f2f
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-02-20 13:06:46 +0530

    Update file6.js

--- JS - 1/file6.js
+++ JS - 1/file6.js
@@ -16,3 +16,14 @@ const colour = x > 10 ? 'red' : 'blue';
 console.log(colour);
 
 // switch case
+switch (colour) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is not red or blue');
+        break;
+}
