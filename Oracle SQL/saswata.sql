077979b1f1d87541b277d8ed4eea6b2930d1b08d
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-02-22 16:04:44 +0530

    users in sql

--- Oracle SQL/saswata.sql
+++ Oracle SQL/saswata.sql
@@ -1,2 +1,21 @@
 CREATE DATABASE DB1
 USER SASWATA IDENTIFIED BY SASWATA;
+
+CREATE TABLE newtable (
+  testvalue NUMBER(4)
+);
+
+desc newtable;
+
+INSERT INTO newtable (testvalue)
+VALUES (1);
+
+create table priv_test (
+    col1 date
+);
+
+select sysdate from dual;
+
+insert into priv_test values(sysdate);
+select * from priv_test;
+
