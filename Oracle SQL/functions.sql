2b7299934ba12690e3482e613428909a9b83f61f
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-02-28 20:54:21 +0530

    Update functions.sql

--- Oracle SQL/functions.sql
+++ Oracle SQL/functions.sql
@@ -13,3 +13,12 @@ SELECT first_name, UPPER(first_name),
        email, INITCAP(email),
        UPPER('bmw i8')FROM employees
 WHERE job_id = 'IT_PROG';
+
+select instr('I am learning how to use functions in Oracle sql', 'o', -1, 1)
+from dual;
+
+select first_name, instr(first_name, 'a')
+from employees;
+
+select first_name, replace(first_name, 'a') as replaced_output
+from employees;
