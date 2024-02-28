e5cd7a1bec07c0745e63703691d403fd15fc13d7
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-02-28 14:00:22 +0530

    character functions

--- Oracle SQL/functions.sql
+++ Oracle SQL/functions.sql
@@ -4,3 +4,12 @@ info employees;
 
 select upper(first_name), lower(last_name), initcap(email)
 from employees;
+
+select first_name || ' ' || last_name
+from employees;
+
+SELECT first_name, UPPER(first_name), 
+       last_name, LOWER(last_name), 
+       email, INITCAP(email),
+       UPPER('bmw i8')FROM employees
+WHERE job_id = 'IT_PROG';
