062f553790a7cb60cd0f11adc4618179cff67721
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-02-29 21:01:18 +0530

    date functions & arithmetic operations

--- Oracle SQL/functions.sql
+++ Oracle SQL/functions.sql
@@ -42,3 +42,16 @@ select substr('John Jacob', instr('John Jacob', ' ')+1,
                 length('John Jacob')) as "last name"   -- length function is optional here
 from dual;
 
+select sysdate, current_date, sessiontimezone, systimestamp, current_timestamp
+from dual;
+
+select sysdate, sysdate+1, (sysdate + (1/24)), (sysdate + (1 / (24*60))) from dual;
+
+select max(salary), years_since_hired
+from ( select employee_id, salary, manager_id, hire_date, sysdate, floor(sysdate - hire_date) as days_since_hired,
+        round( floor(sysdate - hire_date) / 365 ) as years_since_hired
+from employees )
+--where years_since_hired > 19
+group by years_since_hired;
+
+select * from employees;
