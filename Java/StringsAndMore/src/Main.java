be0d3cfa975025c13c7eb141d7b1caa5d6d440e1
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-04 23:46:22 +0530

    string

---
+++ Java/StringsAndMore/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 1, 3, 4};
+        int a = 10;
+        String name = "Saswata Mishra";
+        System.out.println(name);
+    }
+}
