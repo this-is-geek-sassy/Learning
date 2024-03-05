436422f832ffcd1fdde61fee88172d228ca7bb22
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-05 21:12:26 +0530

    inline views

--- Oracle SQL/nested_subqueries.sql
+++ Oracle SQL/nested_subqueries.sql
@@ -14,8 +14,17 @@ where salary > any (SELECT salary
                     FROM employees
                     WHERE first_name = 'John');
                 
-select distinct employee_id from employees
+select distinct employee_id, first_name, last_name from employees
 where hire_date = any (select max(hire_date)
                     from employees
                     group by employees.department_id);
                     
+-- Inline views
+select e.employee_id, e.first_name, e.last_name, b.department_name, b.city, b.state_province
+from employees e JOIN (SELECT department_id, department_name, state_province, city
+                        FROM departments JOIN locations
+                        USING(location_id)
+                        ORDER BY department_id) b
+USING (department_id);
+
+
