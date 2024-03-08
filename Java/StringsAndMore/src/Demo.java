451b37cb448ed64cc1bae69ef4b2c3e963197214
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-08 11:25:37 +0530

    this keyword

--- Java/StringsAndMore/src/Demo.java
+++ Java/StringsAndMore/src/Demo.java
@@ -1,13 +1,20 @@
 public class Demo {
 
+    int num;
     static Demo obj;
     static {
         System.out.println(Demo.obj);
         Demo.obj = new Demo();
         System.out.println(Demo.obj);
     }
+    Demo() {
+        System.out.println("this inside the def constructor : " + this);
+        System.out.println("this.x : " + this.num);
+    }
 
     public static void main(String[] args) {
         System.out.println(Demo.obj);
+        new Demo();
+        new Demo();
     }
 }
