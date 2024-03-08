2e24843943c4e98e8b30092cdda10667a6a876fa
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-08 11:15:53 +0530

    static block and non static block
    and their flow of execution in a class

--- Java/StringsAndMore/src/StaticNonStatic.java
+++ Java/StringsAndMore/src/StaticNonStatic.java
@@ -1,10 +1,21 @@
 public class StaticNonStatic {
 
     static int num;
+    int anotherNum;
+
+    // non-static constructor
+    StaticNonStatic() {
+        System.out.println("Inside the constructor");
+    }
+    {
+        System.out.println("Inside the non-static block");
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println(StaticNonStatic.num);   // will definitely return zero each time
+        StaticNonStatic nonStatic = new StaticNonStatic();
+        System.out.println("nonStatic.anotherNum : " + nonStatic.anotherNum);
     }
     static {
         System.out.println("One static block");
