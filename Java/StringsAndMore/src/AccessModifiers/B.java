d7877cf959f33432602ad61b2feb7eead93cbda2
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-08 16:03:08 +0530

    Create B.java

---
+++ Java/StringsAndMore/src/AccessModifiers/B.java
@@ -0,0 +1,11 @@
+package AccessModifiers;
+
+public class B {
+    public static void main(String[] args) {
+        A aObj = new A();
+//        System.out.println(aObj.a);
+        System.out.println(aObj.b);
+        System.out.println(aObj.c);
+        System.out.println(aObj.d);
+    }
+}
