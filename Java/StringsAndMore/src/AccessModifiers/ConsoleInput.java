5c4e88ef361104e9e806e57c1d7a1f11a850d41f
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-09 08:55:57 +0530

    static imports in java

--- Java/StringsAndMore/src/AccessModifiers/ConsoleInput.java
+++ Java/StringsAndMore/src/AccessModifiers/ConsoleInput.java
@@ -1,13 +1,18 @@
 package AccessModifiers;
 
 import java.util.Scanner;
+import static java.lang.System.out;
+import static java.lang.Integer.*;
+import static java.lang.Math.*;
 
 public class ConsoleInput {
     public static void main(String[] args) {
-        System.out.println("Enter two numbers: ");
+        out.println("Enter two numbers: ");
         Scanner scanner = new Scanner(System.in);
-        int num1 = Integer.parseInt(scanner.next());
+        int num1 = parseInt(scanner.next());
         int num2 = scanner.nextInt();
         System.out.println("Output is: " + (num1+num2));
+        out.println(sqrt(36));
+        out.println(pow(num1, num2));
     }
 }
