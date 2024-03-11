0bc61e37cfbe021e16e70bb023b603e58bf79248
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-11 13:15:51 +0530

    range missing

--- Java/Problems/src/FindMissingNumber.java
+++ Java/Problems/src/FindMissingNumber.java
@@ -1,13 +1,40 @@
+import java.util.ArrayList;
+
 public class FindMissingNumber {
     public static void main(String[] args) {
-        int[] array = {9,2,4,6,3,7,5,0,1,10,8,12};
-        int size = array.length;
-        long desiredSum = (size * (size + 1)) / 2;
+        int[] array = {9,2,4,6,3,7,5,1,0,10,8,12};
+        ArrayList<Integer> minMax;
+
+        minMax = findMinMax(array);
+        int min = minMax.get(0), max = minMax.get(1), size = max - min + 1;
+        long desiredSum = ((max * (max + 1)) / 2) - (((min-1) * min) / 2);
         long actualSum=0;
         for (int i = 0; i < array.length; i++) {
             actualSum += array[i];
         }
         int missingElement = (int)(desiredSum - actualSum);
         System.out.println(missingElement);
+    }
+    public static ArrayList<Integer> findMinMax(int[] array) {
+        int min, max;
+        if (array[0] < array[1]) {
+            min = array[0];
+            max = array[1];
+        }
+        else {
+            min = array[1];
+            max = array[0];
+        }
+        for (int i = 2; i < array.length; i++) {
+            if (array[i] < min)
+                min = array[i];
+            else if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(min);
+        list.add(max);
+        return list;
     }
 }
