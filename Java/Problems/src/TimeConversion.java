3b2bd4800e1272ba1d2a1b8238fe59830f93f738
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-11 11:32:13 +0530

    both ways format conversion

--- Java/Problems/src/TimeConversion.java
+++ Java/Problems/src/TimeConversion.java
@@ -3,8 +3,42 @@ import java.util.Scanner;
 public class TimeConversion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int flag;
+        do {
+            System.out.println("Enter 1 to convert 12 hour format to 24 hour format" +
+                    "\nAnd 2 to convert from 24 hour format to 12 hour format");
+            flag = scanner.nextInt();
+        } while (flag != 1 && flag!=2);
+
+        if (flag == 1) {
+            _12to24hoursConverter(scanner);
+        } else {
+            _24to12hoursConverter(scanner);
+        }
+    }
+
+    public static boolean check12hourFormat(int hour, int minute, int second, String morningEvening) {
+        if (!(hour > 0 && minute > 0 && second > 0))
+            return false;
+        if (hour > 12 || minute >= 60 || second >= 60 ||
+                !(morningEvening.toLowerCase().equals("am") || morningEvening.toUpperCase().equals("PM"))) {
+            return false;
+        }
+        return true;
+    }
+    public static boolean check24hourFormat(int hour, int minute, int second) {
+        if (!(hour > 0 && minute > 0 && second > 0))
+            return false;
+        if (hour > 23 || minute >= 60 || second >= 60) {
+            return false;
+        }
+        return true;
+    }
+    public static void _12to24hoursConverter (Scanner scanner) {
         int hour, minute, second;
         String morningEvening;
+
         System.out.println("Please enter the time in 12 hour format, " +
                 "separated by space and ending with \"AM\" or \"PM\" ");
         hour = scanner.nextInt();
@@ -12,7 +46,7 @@ public class TimeConversion {
         second = scanner.nextInt();
         morningEvening = scanner.next();
         System.out.println(hour + " " + minute + " " + second + " " + morningEvening);
-        if (!check(hour, minute, second, morningEvening)) {
+        if (!check12hourFormat(hour, minute, second, morningEvening)) {
             System.out.println("Bad input");
             return;
         }
@@ -21,12 +55,31 @@ public class TimeConversion {
         }
         System.out.println(hour + " " + minute + " " + second);
     }
+    public static void _24to12hoursConverter (Scanner scanner) {
+        int hour, minute, second;
+        String morningEvening;
 
-    public static boolean check(int hour, int minute, int second, String morningEvening) {
-        if (hour > 12 || minute >= 60 || second >= 60 ||
-                !(morningEvening.equals("AM") || morningEvening.equals("PM"))) {
-            return false;
+        System.out.println("Please enter the time in 24 hour format: ");
+        hour = scanner.nextInt();
+        minute = scanner.nextInt();
+        second = scanner.nextInt();
+        System.out.println(hour + " " + minute + " " + second);
+
+        if (!check24hourFormat(hour, minute, second)) {
+            System.out.println("Bad input");
+            return;
         }
-        return true;
+        if (hour >=13 && hour <= 23) {
+            hour -= 12;
+            morningEvening = "PM";
+        } else if (hour == 12) {
+            morningEvening = "PM";
+        } else if (hour == 0) {
+            hour = 12;
+            morningEvening = "AM";
+        } else {
+            morningEvening = "AM";
+        }
+        System.out.println(hour + " " + minute + " " + second + " " + morningEvening);
     }
 }
