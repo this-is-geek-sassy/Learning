5e5d28cf4ca340c07aa2c353c345dfdd13516218
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-11 18:25:49 +0530

    super keyword

--- Java/Inheritance/src/Child.java
+++ Java/Inheritance/src/Child.java
@@ -2,7 +2,10 @@ public class Child extends Parent{
     Child() {
         System.out.println("Child object " + this);
     }
+    @Override
     void f2() {
+        super.f2();
+        System.out.println(this);
         System.out.println("inside f2");
     }
 }
--- Java/Inheritance/src/Parent.java
+++ Java/Inheritance/src/Parent.java
@@ -5,4 +5,7 @@ public class Parent {
     void f1() {
         System.out.println("inside f1");
     }
+    void f2() {
+        System.out.println("inside parent's s2 method");
+    }
 }
