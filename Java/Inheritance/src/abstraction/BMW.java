4b0007d393af5d79eac38d9eff4eacaca862b893
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-12 12:23:52 +0530

    abstract class

---
+++ Java/Inheritance/src/abstraction/BMW.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+public abstract class BMW {
+    void commonFunc() {
+        System.out.println("inside common functionality method");
+    }
+    abstract void accelerate();
+
+    public static void main(String[] args) {
+        System.out.println("inside the main method");
+    }
+}
