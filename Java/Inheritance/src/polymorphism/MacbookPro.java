2d504434cbd36f461c6a403906c3d6a467ade61c
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-12 18:51:13 +0530

    runtime polymorphism with interfaces
    and method overriding

--- Java/Inheritance/src/polymorphism/MacbookPro.java
+++ Java/Inheritance/src/polymorphism/MacbookPro.java
@@ -10,4 +10,7 @@ public class MacbookPro extends Macbook{
     public void shutdown() {
         System.out.println("Macbook pro shutting down");
     }
+    public void screenGlowUp() {
+        System.out.println("Screen is running beautiful screensavers");
+    }
 }
--- Java/Inheritance/src/polymorphism/Test.java
+++ Java/Inheritance/src/polymorphism/Test.java
@@ -2,10 +2,15 @@ package polymorphism;
 
 public class Test {
     public static void main(String[] args) {
-        AppleLaptop m1 = new MacbookPro();
-        AppleLaptop m2 = new MacbookAir();
+        AppleLaptop m1 = new MacbookPro();   // implicit "upcasting"
+        AppleLaptop m2 = new MacbookAir();   // implicit "upcasting"
+
+        MacbookPro m3 = (MacbookPro) m1;      // explicit down-casting
 
         m1.start();  m1.shutdown();
         m2.start();  m2.shutdown();
+
+        ((MacbookPro) m1).screenGlowUp();
+//        m1.screenGlowUp();   // gives error
     }
 }
