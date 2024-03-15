965252ab776c212e3edfdd64897d62d450eb6d73
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-15 17:16:27 +0530

    throw Exception

---
+++ Java/Inheritance/src/exception/ThrowKeyword.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class ThrowKeyword {
+    public static void main(String[] args) throws Exception {
+
+//        throw new RuntimeException("Funds not available");
+        throw new Exception("Funds not available");
+    }
+}
