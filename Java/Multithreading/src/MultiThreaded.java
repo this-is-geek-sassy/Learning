b722d3c570ecde0aa15db19055039028ff22fcfa
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-16 21:49:33 +0530

    thread names (get & set)

--- Java/Multithreading/src/MultiThreaded.java
+++ Java/Multithreading/src/MultiThreaded.java
@@ -5,24 +5,33 @@ public class MultiThreaded extends Thread {
         MultiThreaded mt = new MultiThreaded();
         mt.start();
 
+        Thread currentThread = Thread.currentThread();
+        System.out.println("thread name is: " + currentThread.getName());
+        /*
         for (int j = 1; j <= 200; j++) {
-
             System.out.println("\t j = " + j);
             Thread.sleep((int)(Math.random()*1000));
         }
+        */
+
 //        System.out.println();
     }
 
     public void run() {
-        for (int i = 1; i <= 200; i++) {
 
-            System.out.println("\t i = " + i);
-            try {
-                Thread.sleep((int)(Math.random()*1000));
-            } catch (InterruptedException e) {
-                System.out.println("Child thread exiting");
-            }
-        }
+        Thread currentThread = Thread.currentThread();
+        currentThread.setName("Print 200 numbers");
+        System.out.println("thread name is: " + currentThread.getName());
+
+//        for (int i = 1; i <= 200; i++) {
+//
+//            System.out.println("\t i = " + i);
+//            try {
+//                Thread.sleep((int)(Math.random()*1000));
+//            } catch (InterruptedException e) {
+//                System.out.println("Child thread exiting");
+//            }
+//        }
 //        System.out.println();
     }
 
