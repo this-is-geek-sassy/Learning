74ecbb7a0b58e8fa4cda143ceecc94998e52f3bb
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-16 21:00:52 +0530

    sleep method in threads

--- Java/Multithreading/src/MultiThreaded.java
+++ Java/Multithreading/src/MultiThreaded.java
@@ -1,23 +1,29 @@
 public class MultiThreaded extends Thread {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         MultiThreaded mt = new MultiThreaded();
         mt.start();
 
         for (int j = 1; j <= 200; j++) {
 
-            System.out.print("\t j = " + j);
+            System.out.println("\t j = " + j);
+            Thread.sleep((int)(Math.random()*1000));
         }
-        System.out.println();
+//        System.out.println();
     }
 
     public void run() {
         for (int i = 1; i <= 200; i++) {
 
-            System.out.print("\t i = " + i);
+            System.out.println("\t i = " + i);
+            try {
+                Thread.sleep((int)(Math.random()*1000));
+            } catch (InterruptedException e) {
+                System.out.println("Child thread exiting");
+            }
         }
-        System.out.println();
+//        System.out.println();
     }
 
 }
