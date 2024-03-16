c842a8960252fd884bf054bc1b212ea91b69d1af
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-16 21:53:19 +0530

    thread priority

--- Java/Multithreading/src/MultiThreaded.java
+++ Java/Multithreading/src/MultiThreaded.java
@@ -3,10 +3,17 @@ public class MultiThreaded extends Thread {
     public static void main(String[] args) throws InterruptedException {
 
         MultiThreaded mt = new MultiThreaded();
+        mt.setPriority(MAX_PRIORITY);
+        mt.setName("mt");
         mt.start();
 
+        MultiThreaded mt1 = new MultiThreaded();
+        mt1.setPriority(MIN_PRIORITY);
+        mt1.setName("mt1");
+        mt1.start();
+
         Thread currentThread = Thread.currentThread();
-        System.out.println("thread name is: " + currentThread.getName());
+//        System.out.println("thread name is: " + currentThread.getName());
         /*
         for (int j = 1; j <= 200; j++) {
             System.out.println("\t j = " + j);
@@ -18,10 +25,11 @@ public class MultiThreaded extends Thread {
     }
 
     public void run() {
+        System.out.println("Thread name:" + Thread.currentThread().getName());
 
-        Thread currentThread = Thread.currentThread();
-        currentThread.setName("Print 200 numbers");
-        System.out.println("thread name is: " + currentThread.getName());
+//        Thread currentThread = Thread.currentThread();
+//        currentThread.setName("Print 200 numbers");
+//        System.out.println("thread name is: " + currentThread.getName());
 
 //        for (int i = 1; i <= 200; i++) {
 //
