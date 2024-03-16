1fbb23459d3c007aef7fe4e37568e5e6b4ad8cf1
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-16 21:12:28 +0530

    join method of thread

---
+++ Java/Multithreading/src/JoinDemo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class JoinDemo extends Thread {
+    static int n, sum=0;
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Sum of first n numbers");
+        System.out.println("Enter a value for n:");
+        Scanner scanner = new Scanner(System.in);
+//        n = scanner.nextInt();
+        JoinDemo.n = scanner.nextInt();
+
+        JoinDemo jd = new JoinDemo();
+        jd.start();
+        jd.join();
+
+        System.out.println("Sum of first " + JoinDemo.n +" numbers: " + JoinDemo.sum);
+    }
+    public void run() {
+        for (int i = 1; i <= JoinDemo.n; i++) {
+            JoinDemo.sum += i;
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
