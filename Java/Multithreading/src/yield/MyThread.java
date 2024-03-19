e2c2b3591b01251444c9e5fd26df0479793749be
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-19 10:10:46 +0530

    yield method

---
+++ Java/Multithreading/src/yield/MyThread.java
@@ -0,0 +1,10 @@
+package yield;
+
+public class MyThread extends Thread{
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Child thread");
+            Thread.yield();
+        }
+    }
+}
---
+++ Java/Multithreading/src/yield/YieldDemo.java
@@ -0,0 +1,12 @@
+package yield;
+
+public class YieldDemo {
+    public static void main(String[] args) {
+        MyThread t = new MyThread();
+        t.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Main thread");
+        }
+    }
+}
