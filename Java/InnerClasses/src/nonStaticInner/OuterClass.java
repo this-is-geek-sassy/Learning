e9c3b4d8ca5ec0d602c616df42a42f76cd599d12
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-19 16:47:19 +0530

    accessing outer class's members from inner class

--- Java/InnerClasses/src/nonStaticInner/OuterClass.java
+++ Java/InnerClasses/src/nonStaticInner/OuterClass.java
@@ -1,9 +1,21 @@
 package nonStaticInner;
 
 public class OuterClass {
+    private static int x=50;
+    private int y;
+    public OuterClass(int y) {
+        this.y = y;
+    }
     class Inner {
+        private int y;
+        public Inner(int y) {
+            this.y = y;
+        }
         void f2() {
-            System.out.println("inner class's non-static method");
+//            System.out.println("inner class's non-static method");
+            System.out.println(OuterClass.x);
+            System.out.println(OuterClass.this.y);
+            System.out.println(this.y);
         }
     }
     void f1() {
@@ -11,9 +23,9 @@ public class OuterClass {
     }
 
     public static void main(String[] args) {
-        OuterClass outer = new OuterClass();
+        OuterClass outer = new OuterClass(80);
         outer.f1();
-        OuterClass.Inner inner = outer.new Inner();
+        OuterClass.Inner inner = outer.new Inner(32);
         inner.f2();
     }
 }
