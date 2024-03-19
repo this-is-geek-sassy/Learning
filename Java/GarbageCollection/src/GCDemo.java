0c9374251f94371ffd9a352d7dc2a1d75f910d0b
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-19 16:08:52 +0530

    forcing and requesting garbage collector to run

--- Java/GarbageCollection/src/GCDemo.java
+++ Java/GarbageCollection/src/GCDemo.java
@@ -1,15 +1,22 @@
 public class GCDemo {
-    public GCDemo() {
-        System.out.println(this + " created");
+    private int objID;
+    public GCDemo(int objID) {
+        this.objID = objID;
+        System.out.println("created " + this.objID);
     }
 
     public static void main(String[] args) {
-        GCDemo gcDemo = new GCDemo();
-        GCDemo gcDemo1 = new GCDemo();
+//        GCDemo gcDemo = new GCDemo();
+//        GCDemo gcDemo1 = new GCDemo();
+        new GCDemo(1);
+        new GCDemo(2);
+        for (int i = 1; i <= 100; i++) {
+            System.gc();
+        }
     }
 
     @Override
     protected void finalize() throws Throwable {
-        System.out.println(this + " finalized");
+        System.out.println("finalized " + this.objID);
     }
 }
