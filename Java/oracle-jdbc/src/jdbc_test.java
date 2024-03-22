ec1033b9666083f943dde4d38cfb030f688c82ce
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-22 11:49:57 +0530

    Update jdbc_test.java

--- Java/oracle-jdbc/src/jdbc_test.java
+++ Java/oracle-jdbc/src/jdbc_test.java
@@ -1,10 +1,6 @@
 //package jdbc_test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class jdbc_test {
 
@@ -22,6 +18,12 @@ public class jdbc_test {
             System.out.println("Connection successful");
 
             mySmt = myConn.createStatement();
+            PreparedStatement myPrepStatement = myConn.prepareStatement("" +
+                    "delete from employees_duplicate " +
+                    "where first_name=? and last_name=?");
+            myPrepStatement.setString(1, "Eric");
+            myPrepStatement.setString(2, "Wright");
+
             long rowsAffectedByInsert = mySmt.executeLargeUpdate("" +
                     "insert into employees_duplicate " +
                     "(last_name, first_name, email, department_id, salary, hire_date, job_id)" +
@@ -35,9 +37,10 @@ public class jdbc_test {
                                         "from dual) " +
                     "where first_name='Eric' and last_name='Wright'");
 
-            int rowsAffectedByDelete = mySmt.executeUpdate("" +
-                    "delete from employees_duplicate " +
-                    "where first_name='Eric' and last_name='Wright'");
+//            int rowsAffectedByDelete = mySmt.executeUpdate("" +
+//                    "delete from employees_duplicate " +
+//                    "where first_name='Eric' and last_name='Wright'");
+            int rowsAffectedByDelete = myPrepStatement.executeUpdate();
 
             if (rowsAffectedByInsert >= 1 && rowsAffectedByUpdate >= 1 && rowsAffectedByDelete >= 1) {
                 System.out.println("Updation & deletion successful!");
