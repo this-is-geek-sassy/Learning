9e53edb7f1cb94a2be2c951deac317b144bad969
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-22 11:37:40 +0530

    update & delete through jdbc

--- Java/oracle-jdbc/src/jdbc_test.java
+++ Java/oracle-jdbc/src/jdbc_test.java
@@ -22,16 +22,33 @@ public class jdbc_test {
             System.out.println("Connection successful");
 
             mySmt = myConn.createStatement();
-            long rowsAffected = mySmt.executeLargeUpdate("" +
+            long rowsAffectedByInsert = mySmt.executeLargeUpdate("" +
                     "insert into employees_duplicate " +
                     "(last_name, first_name, email, department_id, salary, hire_date, job_id)" +
                     "values" +
                     "('Wright', 'Eric', 'eric.wright@foo.com', '40', 330000.00, '17-JUN-03', 'HR_REP')");
 
+            int rowsAffectedByUpdate = mySmt.executeUpdate("" +
+                    "update employees_duplicate " +
+                    "set EMPLOYEE_ID=(select (select max(employee_id) " +
+                                        "FROM employees_duplicate) + 1 " +
+                                        "from dual) " +
+                    "where first_name='Eric' and last_name='Wright'");
+
+            int rowsAffectedByDelete = mySmt.executeUpdate("" +
+                    "delete from employees_duplicate " +
+                    "where first_name='Eric' and last_name='Wright'");
+
+            if (rowsAffectedByInsert >= 1 && rowsAffectedByUpdate >= 1 && rowsAffectedByDelete >= 1) {
+                System.out.println("Updation & deletion successful!");
+            }
+
             myRes = mySmt.executeQuery("select * from employees_duplicate");
 
             while (myRes.next()) {
-                System.out.println(myRes.getString("last_name") + ", " + myRes.getString("first_name"));
+                System.out.println(myRes.getString("employee_id") + ", "
+                        + myRes.getString("last_name") + ", "
+                        + myRes.getString("first_name"));
             }
         }catch (Exception exc) {
             exc.printStackTrace();
--- Oracle SQL/HR.sql
+++ Oracle SQL/HR.sql
@@ -13,3 +13,8 @@ insert into employees_duplicate
                     (last_name, first_name, email, department_id, salary, hire_date, job_id)
                     values
                     ('Wright', 'Eric', 'eric.wright@foo.com', '40', 330000.00, '17-JUN-03', 'HR_REP');
+                    
+(select (select max(employee_id)
+FROM employees_duplicate) + 1
+from dual);
+--order BY employee_id;
