b7c904b9331d7b24004b80a1853f3534835c543f
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-03-26 18:48:30 +0530

    updating to make it compatible /w oracle db

--- Java/jdbc-source-code-v2/jdbc-mysql-course-source-code-v2/lecture-09-stored-procedures-part-1/jdbc-stored-procedures-tutorial/sql/table-setup.sql
+++ Java/jdbc-source-code-v2/jdbc-mysql-course-source-code-v2/lecture-09-stored-procedures-part-1/jdbc-stored-procedures-tutorial/sql/table-setup.sql
@@ -2,9 +2,9 @@ create database if not exists demo;
 
 use demo;
 
-drop table if exists employees;
+drop table if exists employees2;
 
-CREATE TABLE `employees` (
+CREATE TABLE `employees2` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `last_name` varchar(64) DEFAULT NULL,
   `first_name` varchar(64) DEFAULT NULL,
@@ -14,21 +14,21 @@ CREATE TABLE `employees` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (1,'Doe','John','john.doe@foo.com', 'HR', 55000.00);
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (2,'Public','Mary','mary.public@foo.com', 'Engineering', 75000.00);
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (3,'Queue','Susan','susan.queue@foo.com', 'Legal', 130000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (1,'Doe','John','john.doe@foo.com', 'HR', 55000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (2,'Public','Mary','mary.public@foo.com', 'Engineering', 75000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (3,'Queue','Susan','susan.queue@foo.com', 'Legal', 130000.00);
 
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (4,'Williams','David','david.williams@foo.com', 'HR', 120000.00);
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (5,'Johnson','Lisa','lisa.johnson@foo.com', 'Engineering', 50000.00);
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (6,'Smith','Paul','paul.smith@foo.com', 'Legal', 100000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (4,'Williams','David','david.williams@foo.com', 'HR', 120000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (5,'Johnson','Lisa','lisa.johnson@foo.com', 'Engineering', 50000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (6,'Smith','Paul','paul.smith@foo.com', 'Legal', 100000.00);
 
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (7,'Adams','Carl','carl.adams@foo.com', 'HR', 50000.00);
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (8,'Brown','Bill','bill.brown@foo.com', 'Engineering', 50000.00);
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (9,'Thomas','Susan','susan.thomas@foo.com', 'Legal', 80000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (7,'Adams','Carl','carl.adams@foo.com', 'HR', 50000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (8,'Brown','Bill','bill.brown@foo.com', 'Engineering', 50000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (9,'Thomas','Susan','susan.thomas@foo.com', 'Legal', 80000.00);
 
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (10,'Davis','John','john.davis@foo.com', 'HR', 45000.00);
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (11,'Fowler','Mary','mary.fowler@foo.com', 'Engineering', 65000.00);
-INSERT INTO `employees` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (12,'Waters','David','david.waters@foo.com', 'Legal', 90000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (10,'Davis','John','john.davis@foo.com', 'HR', 45000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (11,'Fowler','Mary','mary.fowler@foo.com', 'Engineering', 65000.00);
+INSERT INTO `employees2` (`id`,`last_name`,`first_name`,`email`, `department`, `salary`) VALUES (12,'Waters','David','david.waters@foo.com', 'Legal', 90000.00);
 
 --
 -- DEFINE STORED PROCEDURES
@@ -40,18 +40,18 @@ DROP PROCEDURE IF EXISTS `get_count_for_department`$$
 CREATE DEFINER=`student`@`localhost` PROCEDURE `get_count_for_department`(IN the_department VARCHAR(64), OUT the_count INT)
 BEGIN
 	
-	SELECT COUNT(*) INTO the_count FROM employees where department=the_department;
+	SELECT COUNT(*) INTO the_count FROM employees2 where department=the_department;
 
 END$$
 DELIMITER ;
 
 DELIMITER $$
-DROP PROCEDURE IF EXISTS `get_employees_for_department`$$
+DROP PROCEDURE IF EXISTS `get_employees2_for_department`$$
 
-CREATE DEFINER=`student`@`localhost` PROCEDURE `get_employees_for_department`(IN the_department VARCHAR(64))
+CREATE DEFINER=`student`@`localhost` PROCEDURE `get_employees2_for_department`(IN the_department VARCHAR(64))
 BEGIN
 
-	SELECT * from employees where department=the_department;
+	SELECT * from employees2 where department=the_department;
 
 END$$
 DELIMITER ;
@@ -74,7 +74,7 @@ DROP PROCEDURE IF EXISTS `increase_salaries_for_department`$$
 CREATE DEFINER=`student`@`localhost` PROCEDURE `increase_salaries_for_department`(IN the_department VARCHAR(64), IN increase_amount DECIMAL(10,2))
 BEGIN
 
-	UPDATE employees SET salary= salary + increase_amount where department=the_department;
+	UPDATE employees2 SET salary= salary + increase_amount where department=the_department;
 
 END$$
 DELIMITER ;
