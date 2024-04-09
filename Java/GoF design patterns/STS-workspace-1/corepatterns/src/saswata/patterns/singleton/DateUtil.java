bba11888f920d77c471792110b8d0d267b1c6335
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-09 21:46:26 +0530

    implementation of thread safety mechanisms

--- Java/GoF design patterns/STS-workspace-1/corepatterns/src/saswata/patterns/singleton/DateUtil.java
+++ Java/GoF design patterns/STS-workspace-1/corepatterns/src/saswata/patterns/singleton/DateUtil.java
@@ -4,21 +4,26 @@ public class DateUtil {
 	
 	// Eager initialization
 	// private static DateUtil instance = new DateUtil();
-	private static DateUtil instance;
+	private static volatile DateUtil instance;
 	
-	static {
-		instance = new DateUtil();
-	}
+//	static {
+//		instance = new DateUtil();
+//	}
 	
 	private DateUtil() {
 		
 	}
 	
+//	public static synchronized DateUtil getInstance() {
 	public static DateUtil getInstance() {
 		
-		// Lazy initialization
-//		if (instance == null)
-//			instance = new DateUtil();
+		if (instance == null) {
+			synchronized (DateUtil.class) {
+				// Lazy initialization
+				if (instance == null)
+					instance = new DateUtil();
+			}
+		}
 		
 		return instance;
 	}
