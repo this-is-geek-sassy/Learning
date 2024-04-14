c83d0774f9366a9e2d5faaa2a47005ac00d29d63
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-14 11:44:18 +0530

    using iterator over array list

--- Java/CollectionsWithGenerics/src/list/ArrayListDemo.java
+++ Java/CollectionsWithGenerics/src/list/ArrayListDemo.java
@@ -1,14 +1,28 @@
 package list;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Objects;
 
 public class ArrayListDemo {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList();
+//        ArrayList<Integer> list = new ArrayList();
+        ArrayList list = new ArrayList();
         list.add(Integer.valueOf(20));
         list.add(10); // autoboxing
-//        list.add(new Integer[]{10, 20, 30});
-//        list.add(30.45);
+        list.add(new Integer[]{10, 20, 30});
+        list.add(30.45);
+
+        Iterator itr = list.iterator();
+        while (itr.hasNext()) {
+            Object o = itr.next();
+            System.out.println(o);
+            if (o.getClass().isArray()){
+                for (int element : (int[])o) {
+                    System.out.println("inner array element: " + element);
+                }
+            }
+        }
 
         System.out.println(list);
     }
