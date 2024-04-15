de76da0ecf5d3b2c04ca8b0caefdbf6fccf95192
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-15 16:17:32 +0530

    ListIterator

---
+++ Java/CollectionsWithGenerics/src/list/LIDemo.java
@@ -0,0 +1,27 @@
+package list;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class LIDemo {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+
+        list.add("abc");
+        list.add("bef");
+        list.add("xyz");
+
+        ListIterator<String> itr = list.listIterator();
+
+        System.out.println("Traversing thw list in the forward direction");
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        System.out.println("Traversing thw list in the backward direction");
+        while (itr.hasPrevious()) {
+            System.out.println(itr.previous());
+        }
+    }
+}
