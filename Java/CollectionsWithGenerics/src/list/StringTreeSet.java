e3f7d22bd451aeb4bd654233c392b151860d8e5b
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-15 17:01:21 +0530

    string comparator customisation

--- Java/CollectionsWithGenerics/src/list/StringTreeSet.java
+++ Java/CollectionsWithGenerics/src/list/StringTreeSet.java
@@ -1,18 +1,21 @@
 package list;
 
+import comparator.StringComparator;
+
 import java.util.Set;
 import java.util.TreeSet;
 
 public class StringTreeSet {
     public static void main(String[] args) {
-        Set<String> set = new TreeSet<>();
-        set.add("abc");
-        set.add("xyz");
-        set.add("def");
-        set.add("mno");
+        Set<String> set = new TreeSet<>(new StringComparator());
+        set.add("abcfnbuf");
+        set.add("xyzbv");
+        set.add("defjkqhfgqd   fkqojh");
+        set.add("mnok");
 
         for (String elem: set) {
             System.out.println(elem);
         }
+        System.out.println(set);
     }
 }
