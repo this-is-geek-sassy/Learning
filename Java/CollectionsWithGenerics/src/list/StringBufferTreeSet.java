069e2272526b573c7f1940e35711868311ad1b68
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-15 16:55:02 +0530

    stringBufferComparator

--- Java/CollectionsWithGenerics/src/list/StringBufferTreeSet.java
+++ Java/CollectionsWithGenerics/src/list/StringBufferTreeSet.java
@@ -1,11 +1,13 @@
 package list;
 
+import comparator.SBComparator;
+
 import java.util.Set;
 import java.util.TreeSet;
 
 public class StringBufferTreeSet {
     public static void main(String[] args) {
-        Set<StringBuffer> set = new TreeSet<>();
+        Set<StringBuffer> set = new TreeSet<>(new SBComparator());
         set.add(new StringBuffer("abc"));
         set.add(new StringBuffer("xyz"));
         set.add(new StringBuffer("def"));
