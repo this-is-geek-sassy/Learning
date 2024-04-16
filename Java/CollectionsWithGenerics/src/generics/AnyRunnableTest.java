054110c3b485ff79ef72183648dd326113cd01b2
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-16 16:28:25 +0530

    restricting types in custom geneic classes

---
+++ Java/CollectionsWithGenerics/src/generics/AnyRunnableTest.java
@@ -0,0 +1,8 @@
+package generics;
+
+public class AnyRunnableTest {
+    public static void main(String[] args) {
+        AnyRunnable<Thread> t = new AnyRunnable<>();
+//        AnyRunnable<String> t = new AnyRunnable<>();
+    }
+}
