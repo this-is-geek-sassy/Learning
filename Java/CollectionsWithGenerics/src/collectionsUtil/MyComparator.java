741c2653026b872784344fe92069e8048a13daba
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-16 12:36:31 +0530

    custom comparator for sort

---
+++ Java/CollectionsWithGenerics/src/collectionsUtil/MyComparator.java
@@ -0,0 +1,10 @@
+package collectionsUtil;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        return o2.compareTo(o1);
+    }
+}
