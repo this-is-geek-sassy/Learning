7f46d4356b89253762b924ce8a1b61b6108daac6
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-17 11:45:14 +0530

    Simulating list manipulation from multiple threads

--- Java/ConcurrentCollections/src/ArrayListProblems.java
+++ Java/ConcurrentCollections/src/ArrayListProblems.java
@@ -2,11 +2,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-public class ArrayListProblems {
-    public static void main(String[] args) {
+public class ArrayListProblems extends Thread {
 
+    public static CopyOnWriteArrayList<String> courses = new CopyOnWriteArrayList<>();
+//    public static ArrayList<String> courses = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ArrayListProblems arrayListProblems = new ArrayListProblems();
+        arrayListProblems.start();
 //        ArrayList<String> courses = new ArrayList<>();
-        CopyOnWriteArrayList<String> courses = new CopyOnWriteArrayList<>();
+
         courses.add("Java");
         courses.add("Python");
         courses.add("AWS");
@@ -14,13 +20,24 @@ public class ArrayListProblems {
 
         Iterator<String> iterator = courses.iterator();
         while (iterator.hasNext()){
+
+            Thread.sleep(2000);
+
             String course = iterator.next();
             System.out.println(course);
             // courses.add("Node");    // will throw java.util.ConcurrentModificationException
-            if (course.equalsIgnoreCase("docker")) {
-                courses.remove(course);   // again throws java.util.ConcurrentModificationException
-            }
+
         }
         System.out.println(courses);
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        courses.add("kubernetes");
     }
 }
