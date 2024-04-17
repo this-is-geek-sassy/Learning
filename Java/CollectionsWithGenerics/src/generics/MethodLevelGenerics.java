a0172e4a825feed13d86be0486d59bbb5ce8a866
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-17 10:40:41 +0530

    generics at method level

---
+++ Java/CollectionsWithGenerics/src/generics/MethodLevelGenerics.java
@@ -0,0 +1,13 @@
+package generics;
+
+public class MethodLevelGenerics {
+    public <T> void method1(T t) {
+
+    }
+    public <T extends Runnable> void method2 (T t) {
+
+    }
+    public <T extends Runnable&Comparable<String>> void method3(T t) {
+
+    } 
+}
