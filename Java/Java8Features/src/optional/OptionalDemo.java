c68662702637684682f86bbf634189c9f3b3adb3
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-18 13:53:00 +0530

    filter and ifPresent methods

--- Java/Java8Features/src/optional/OptionalDemo.java
+++ Java/Java8Features/src/optional/OptionalDemo.java
@@ -9,11 +9,15 @@ public class OptionalDemo {
         Optional<String> optional = Optional.empty();
         System.out.println("Optional: " + optional);
         System.out.println(optional.isEmpty());
-        System.out.println(optional.get());   // throws NoSuchElementException
+//        System.out.println(optional.get());   // throws NoSuchElementException
 
         Optional<String> data = Optional.of("Optional but very useful");
         System.out.println("data: " + data);
         System.out.println(data.isPresent());
         System.out.println(data.get());
+
+        System.out.println(data.filter((s) -> s.equalsIgnoreCase("optional but very useful")));
+
+        data.ifPresent(System.out::println);
     }
 }
