72da7b5f21b401fa9de7cc18071c2607072d214d
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-18 09:18:19 +0530

    predicate joining

--- Java/Java8Features/src/predicate/PredicateJoins.java
+++ Java/Java8Features/src/predicate/PredicateJoins.java
@@ -5,10 +5,25 @@ import java.util.function.Predicate;
 public class PredicateJoins {
 
     public static void main(String[] args) {
-        int[] x = {0, 10, 20, 30, 40, 50, 60, 70};
+        int[] x = {0, 7, 73, 10, 20, 30, 40, 50, 60, 70, 13, 1};
+
         Predicate<Integer> p1 = i -> (i > 10);
+        Predicate<Integer> p2 = i -> (i%2 == 0);
+
         System.out.println("greater than 10: ");
         method1(p1, x);
+
+        System.out.println("Even numbers: ");
+        method1(p2, x);
+
+        System.out.println("not greater than 10: ");
+        method1(p1.negate(), x);
+
+        System.out.println("numbers that are greater than 10 and even: ");
+        method1(p1.and(p2), x);
+
+        System.out.println("numbers that are greater than 10 or even: ");
+        method1(p1.or(p2), x);
     }
 
     public static void method1(Predicate<Integer> predicate, int[] array) {
