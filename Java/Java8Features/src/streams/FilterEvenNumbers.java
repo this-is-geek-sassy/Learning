bfeb4aabfe00524103e0df2ca6709fa92aeb8ed0
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-18 12:29:28 +0530

    various methods of stream api

--- Java/Java8Features/src/streams/FilterEvenNumbers.java
+++ Java/Java8Features/src/streams/FilterEvenNumbers.java
@@ -1,7 +1,9 @@
 package streams;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class FilterEvenNumbers {
@@ -23,7 +25,20 @@ public class FilterEvenNumbers {
 //        }
 
         List<Integer> l2 = l1.stream().filter(i -> (i%2 == 0)).collect(Collectors.toList());
-
+        long noOfEvenNos = l1.stream().filter(i -> (i%2 == 0)).count();
+        System.out.println("No of even nos: " + noOfEvenNos);
         System.out.println(l2);
+
+        Comparator<Integer> comparator = Integer::compareTo;
+
+        List<Integer> s1 = l1.stream().sorted(comparator).collect(Collectors.toList());
+        System.out.println(s1);
+
+        Integer max = l1.stream().max(comparator).get();
+        System.out.println(max);
+        Integer min = l1.stream().min(comparator).get();
+        System.out.println(min);
+
+        l1.stream().forEach(i -> System.out.println(i));
     }
 }
