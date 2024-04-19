11157727fa17ec155e47de2c83ac2739df876d22
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-19 11:07:15 +0530

    rawtype warning suppression

--- Java/Annotations/src/B.java
+++ Java/Annotations/src/B.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class B {
 
-    @SuppressWarnings({ "deprecation" })
+    @SuppressWarnings({ "deprecation", "rawtypes" })
     public static void main(String[] args) {
 
         A a = new A();
+
+        List list = new ArrayList();
 
         System.out.println(a);
         a.myMethod();
