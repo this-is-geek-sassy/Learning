e7f92b3185ac31c9256302cb99d46436e77fbbcf
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-23 11:19:26 +0530

    listing constructors, fields & methods of a class

--- Java/Reflection/src/reflection/Test.java
+++ Java/Reflection/src/reflection/Test.java
@@ -1,9 +1,19 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+
 public class Test {
 
     public static void main(String[] args) throws ClassNotFoundException {
 
-        System.out.println(Class.forName(Calculator.class.getName()));
+        Class<?> myClass = Class.forName(Calculator.class.getName());
+        System.out.println(myClass.getName());
+        Constructor<?>[] constructors = myClass.getConstructors();
+        System.out.println(Arrays.toString(constructors));
+        // if there is no constructors, it will show o/p as: [public reflection.Calculator()]
+        // if there are only private constructors, it will return an empty array.
+        // if there are public constructors, it will show the array with elements being constructor signatures
+        System.out.println(Arrays.toString(myClass.getMethods()));
     }
 }
