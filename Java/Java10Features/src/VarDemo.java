3635045b9f435964a938fb754a64fae8d5a8c0a5
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-29 18:16:24 +0530

    different uses of var

--- Java/Java10Features/src/VarDemo.java
+++ Java/Java10Features/src/VarDemo.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class VarDemo {
 
     public static void main(String[] args) {
@@ -5,5 +9,15 @@ public class VarDemo {
         float f = 30.0f;
         var name = "saswata";    // inferred type
 //        name = 10;    // type mismatch, as java is a strongly typed language
+
+        Map<String, List<String>> map = new HashMap<>();
+        var map2 = new HashMap<String, List<String>>();
+
+        for (Map.Entry<String, List<String>> entry: map2.entrySet()) {}
+
+        for (var entry: map2.entrySet()) {
+            var value = entry.getValue();
+            System.out.println(value);
+        }
     }
 }
