ba71a0794bedaffff419981abcdd9cc2018b1bf2
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-04-29 18:11:16 +0530

    declaring variables with var

---
+++ Java/Java10Features/src/VarDemo.java
@@ -0,0 +1,9 @@
+public class VarDemo {
+
+    public static void main(String[] args) {
+        int x = 10;
+        float f = 30.0f;
+        var name = "saswata";    // inferred type
+//        name = 10;    // type mismatch, as java is a strongly typed language
+    }
+}
