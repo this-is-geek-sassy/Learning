b8be7470c16f1baa45f32faf7ddb4365c6b182d5
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-05-02 16:11:17 +0530

    chess characters unicode

---
+++ Java/Java12Features/src/unicodeChars/Chess.java
@@ -0,0 +1,16 @@
+package unicodeChars;
+
+public class Chess {
+
+    public static void main(String[] args) {
+
+        System.out.println("\u265A");
+        System.out.println("\u265B");
+
+        System.out.println("\u2654");
+        System.out.println("\u2655");
+
+        System.out.println("\u265C");
+        System.out.println("\u265D");
+    }
+}
