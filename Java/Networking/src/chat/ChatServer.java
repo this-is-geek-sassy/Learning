34299911cd4b4ed03670102b1d9f37a6fb11262d
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-05-04 09:38:00 +0530

    chat server

---
+++ Java/Networking/src/chat/ChatServer.java
@@ -0,0 +1,44 @@
+package chat;
+
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(9090);
+            Socket socket = serverSocket.accept();
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String messageReceived = "", messageToSend = "";
+            while (true) {
+                messageReceived = in.readUTF();
+                System.out.println("client says: " + messageReceived);
+
+                if (messageReceived.equalsIgnoreCase("exit")) {
+                    break;
+                }
+
+                messageToSend = bufferedReader.readLine();
+                out.writeUTF(messageToSend);
+
+                out.flush();
+            }
+
+            bufferedReader.close();
+            out.close();
+            in.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
