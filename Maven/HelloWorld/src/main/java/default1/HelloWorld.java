ca33f026f784d9823d7da724b133d7c04544ed3f
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-05-08 12:55:49 +0530

    compile plugin

---
+++ Maven/HelloWorld/src/main/java/default1/HelloWorld.java
@@ -0,0 +1,12 @@
+package default1;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world!!");
+        System.out.println(StringUtils.capitalize("hello world!"));
+    }
+}
