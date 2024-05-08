a687971eda0644af8170f725841c53f8d1d63d6a
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-05-08 13:12:53 +0530

    Surefire plugin

---
+++ Maven/HelloWorld/src/test/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+public class HelloWorldTest {
+
+    public void testHello() {
+        System.out.println("TEST - hello!! ...");
+    }
+
+    public void testWorld() {
+        System.out.println("TEST - world!! ...");
+    }
+}
