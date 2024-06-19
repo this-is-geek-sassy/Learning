f5bd94f2537331852f57af739a671ae074b5fd36
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-06-19 18:00:33 +0530

    js refresher code sandbox download

---
+++ js_refresher_sandbox/assets/scripts/app.js
@@ -0,0 +1,50 @@
+// import { apiKey } from "./util";
+
+// import apiKey from "./util";
+// import { name, age } from "./util";
+
+import * as utils from "./util";
+
+console.log(utils.apiKey); // default import
+console.info(utils.name, utils.age);
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greet() {
+    console.log("Hello");
+  }
+}
+
+const u1 = new User("Emanuel", 39);
+console.log(u1);
+
+const hobbies = ["sports", "movies", "redaing"];
+
+console.log(hobbies[0]);
+const editedHobbies = hobbies.map((item) => ({ text: item }));
+console.log(editedHobbies);
+
+const [firstName, lastName] = ["saswata", "mishra"];
+console.log(firstName);
+console.log(lastName);
+
+const user = {
+  name: "saswata",
+  age: 34,
+};
+
+const { name: userName, age } = user;
+console.log(userName, age);
+
+const newHobbies = ["cooking"];
+const allHobies = [...hobbies, ...newHobbies];
+console.log(allHobies);
+
+const extendedUser = {
+  isAdmin: true,
+  ...user 
+};
+console.log(extendedUser);
---
+++ js_refresher_sandbox/assets/scripts/util.js
@@ -0,0 +1,6 @@
+export let apiKey = "lsvbsjivgi12314";
+
+// default export
+export default "lsvb______sjivgi12314";
+export let name = "saswata";
+export let age = 28;
