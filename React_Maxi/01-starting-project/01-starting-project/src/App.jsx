c3f8235f005b883ada69379d5920881621f36b52
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-06-22 13:48:28 +0530

    component code refactoring

--- React_Maxi/01-starting-project/01-starting-project/src/App.jsx
+++ React_Maxi/01-starting-project/01-starting-project/src/App.jsx
@@ -1,38 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
 import componentImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomIndex(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-
-  const description = reactDescriptions[genRandomIndex(2)];
-
-  return(
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcepts({image, title, description}) {
-  return(
-    <li>
-      <img src={image} alt={title} srcset="" />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from './components/Header';
+import CoreConcepts from './components/CoreConcepts';
 
 function App() {
   return (
