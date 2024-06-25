d97eb93d097ca4c126949bb9e2866cc02a4f3d8a
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-06-25 17:01:30 +0530

    reacting to events

--- React_Maxi/01-starting-project/01-starting-project/src/components/TabButton.jsx
+++ React_Maxi/01-starting-project/01-starting-project/src/components/TabButton.jsx
@@ -1,6 +1,14 @@
 export default function TabButton({children}) {
 
+    // document.querySelector('button').addEventListener('click', () => {
+
+    // });
+
+    function handleClick() {
+        console.log("hello world");
+    }
+
     return (
-        <li><button>{children}</button></li>
+        <li><button onClick={handleClick}>{children}</button></li>
     );
 }
