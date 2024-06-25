199b0445f24a8c24c84590494ffb95816010fb45
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-06-25 16:55:53 +0530

    tabButton component

--- React_Maxi/01-starting-project/01-starting-project/src/App.jsx
+++ React_Maxi/01-starting-project/01-starting-project/src/App.jsx
@@ -2,6 +2,7 @@ import componentImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header';
 import CoreConcepts from './components/CoreConcepts';
+import TabButton from './components/TabButton';
 
 function App() {
   return (
@@ -18,7 +19,15 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[2]} />
           </ul>
         </section>
-        
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Component</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
