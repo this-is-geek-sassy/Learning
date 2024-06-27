05ef9f257f29fe540ca690c665a8d76ec2411328
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-06-27 13:01:50 +0530

    dynamic tab content base on button clicks

--- React_Maxi/01-starting-project/01-starting-project/src/components/CoreConcepts.jsx
+++ React_Maxi/01-starting-project/01-starting-project/src/components/CoreConcepts.jsx
@@ -1,7 +1,7 @@
 export default function CoreConcepts({image, title, description}) {
     return(
         <li>
-            <img src={image} alt={title} srcset="" />
+            <img src={image} alt={title} srcSet="" />
             <h3>{title}</h3>
             <p>{description}</p>
         </li>
