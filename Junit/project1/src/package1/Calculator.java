d7434174ede927e12e51817d62ab00d6a6d25b6f
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-07-27 12:14:40 +0530

    empty test class creation

---
+++ Junit/project1/src/package1/Calculator.java
@@ -0,0 +1,8 @@
+package package1;
+
+public class Calculator {
+
+    public int integerDivision(int dividend, int divisor) {
+        return dividend/divisor;
+    }
+}
---
+++ Junit/project1/test/package1/CalculatorTest.java
@@ -0,0 +1,12 @@
+package package1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void integerDivision() {
+    }
+}
