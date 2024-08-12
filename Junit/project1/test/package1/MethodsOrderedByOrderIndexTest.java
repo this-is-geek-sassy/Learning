15c3ceacb2080ce6b9f1df6a6d76f7b95eed3eca
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-08-12 07:35:13 +0530

    instance per class

--- Junit/project1/test/package1/MethodsOrderedByOrderIndexTest.java
+++ Junit/project1/test/package1/MethodsOrderedByOrderIndexTest.java
@@ -1,33 +1,41 @@
 package package1;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-
+import org.junit.jupiter.api.*;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodsOrderedByOrderIndexTest {
+
+    public StringBuilder completed = new StringBuilder("");
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("the state of instance object is " + completed);
+    }
 
     @Test
     @Order(1)
     void testC() {
         System.out.println("running test C");
+        completed.append(1);
     }
     @Test
     @Order(3)
     void testA() {
         System.out.println("running test A");
+        completed.append(3);
     }
 
     @Test
     @Order(2)
     void testD() {
         System.out.println("running test D");
+        completed.append(2);
     }
 
     @Test
     @Order(4)
     void testB() {
         System.out.println("running test B");
+        completed.append(4);
     }
 }
