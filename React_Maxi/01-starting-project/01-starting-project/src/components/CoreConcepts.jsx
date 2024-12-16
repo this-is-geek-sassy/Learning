7284d78a08e136cb48c2b60bdd251494ec6cc70b
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2024-12-16 21:56:28 +0530

    basic correction

--- React_Maxi/01-starting-project/01-starting-project/src/components/CoreConcepts.jsx
+++ React_Maxi/01-starting-project/01-starting-project/src/components/CoreConcepts.jsx
@@ -9,14 +9,14 @@ export default function CoreConcepts() {
             <ul>
                 {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
 
-                {/* <CoreConcepts
+                {/* <CoreConcept
                 title={CORE_CONCEPTS[0].title}
                 description={CORE_CONCEPTS[0].description}
                 image={CORE_CONCEPTS[0].image}
                 />
-                <CoreConcepts {...CORE_CONCEPTS[1]} />
-                <CoreConcepts {...CORE_CONCEPTS[1]} />
-                <CoreConcepts {...CORE_CONCEPTS[2]} /> */}
+                <CoreConcept {...CORE_CONCEPTS[1]} />
+                <CoreConcept {...CORE_CONCEPTS[2]} />
+                <CoreConcept {...CORE_CONCEPTS[3]} /> */}
             </ul>
         </section>
     );
