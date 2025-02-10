f3dfbd24985dea32e5c9379d896443eb032c5e26
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2025-02-10 20:27:13 +0530

    gameboard component

--- React_Maxi/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ React_Maxi/07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import Gameboard from './components/Gameboard';
 
 function App() {
   
@@ -10,6 +11,8 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
+
+        <Gameboard></Gameboard>
       </div>
     </main>
   );
