f9b2e4f4208a73d1adbda2ecb55ec965228c21b8
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2025-08-01 15:37:26 +0530

    cpp started

---
+++ cpp/skelliton.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    // std::cout << "Hey Saswata" << std::endl;
+    cout << "Hey Saswata" << endl;
+    // cin >> x >> y;
+    // cout << "Value of x: " << x << " and y: " << y;
+
+    string s;
+    // cin >> s;
+    getline(cin, s);   // takes entire line as input
+    cout << s << endl;
+
+    char ch;
+    cin >> ch;
+    cout << ch << endl;
+}
