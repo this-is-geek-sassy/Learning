b8b009a0c81375bc5669caca3eafd3feed2c3a44
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2025-08-01 15:49:13 +0530

    arrays in cpp

---
+++ cpp/array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+
+    int arr[5];
+
+    cin >> arr[0] >> arr[1];
+    cout << arr[0] << endl;
+
+    int array[3][5];
+
+    array[0][1] = 76;
+
+    cout << array[0][0] << endl;
+    cout << array[0][1] << endl;
+}
