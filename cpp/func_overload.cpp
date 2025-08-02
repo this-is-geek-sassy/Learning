213e48881694d380eab6c892d61878697091284e
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2025-08-02 14:22:26 +0530

    namespace

--- cpp/func_overload.cpp
+++ cpp/func_overload.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+namespace stuffs
+{
+    int sum (int a, int b) {
+        return a*b;
+    }
+
+    int cout (std::string num) {
+        return std::stoi(num);
+    }
+} // namespace stuffs
+
+
 int sum (int a, int b) {
     return a+b;
 }
@@ -14,4 +26,8 @@ int main() {
 
     cout << sum(1,2) << endl;
     cout << sum(31.5, 12.3) << endl;
+
+    cout << stuffs::sum(2,3) << endl;
+
+    cout << stuffs::cout("345jk") << endl;
 }
