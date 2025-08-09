e36eab6f2e2e163bebdb8a763c568f53eb792489
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2025-08-09 18:13:31 +0530

    pairs

---
+++ cpp/stl/pairs.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    pair<int, int> p = {1, 3};
+
+    cout << p.first << endl;
+    cout << p.second << endl;
+
+    pair<int, pair<int, int>> p2 = {1, {2, 3}};
+    cout << p2.second.first << endl;
+
+    pair<int, int> arr[] = {{1, 2}, {3, 4}, {5, 6}};
+
+    cout << arr[0].first << endl;
+}
