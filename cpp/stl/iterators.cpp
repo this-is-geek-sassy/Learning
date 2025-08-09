f3a7ac0c0f17a8693eac02f18cfd7181bafdd537
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2025-08-09 20:17:18 +0530

    iterators in collection

---
+++ cpp/stl/iterators.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print_vector(vector<int>& v) {    // not making a copy -> O(1)
+
+    cout << "v.size() : " << v.size() << endl;   // v.size() -> O(1)
+
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    
+}
+
+void print_vector_using_iterators(vector<int>& v) {
+
+    cout << "v.size() : " << v.size() << endl;   // v.size() -> O(1)
+    for (vector<int> :: iterator it = v.begin(); it!=v.end(); it++) {     // subtle difference b/w it+1 and it++
+        cout << (*it) << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+
+    vector<int> v = {2, 3, 5, 6, 7};
+    print_vector(v);
+
+    vector<int> :: iterator it = v.begin();
+    cout << (*it) << endl;
+    cout << (*(it + 1)) << endl;
+
+    print_vector_using_iterators(v);
+}
