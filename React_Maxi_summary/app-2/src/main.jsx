02cd293401f755b87cafc0e90247ded31325b747
Saswata Mishra <saswata.mishra.cse21@heritageit.edu.in>
2025-08-19 14:13:28 +0530

    recat router attempt

--- React_Maxi_summary/app-2/src/main.jsx
+++ React_Maxi_summary/app-2/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import NewPost from './components/NewPost.jsx';
+
+const routeConfigObject = createBrowserRouter([
+  {path: '/', element: <App />},
+  {path: '/create-post', element: <NewPost />}
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routeConfigObject}/>
   </StrictMode>,
 )
